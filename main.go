61005c00efb28a067752bbd285130305b6b0ca42
alteration <blogcin@naver.com>
2016-02-02 09:28:27 +0900

    Update License and some changes

--- main.go
+++ main.go
@@ -2,27 +2,29 @@ package main
 
 /*
  * ToTo - Go language proxy server
- * Song Hyeon Sik <blogcin@naver.com> 2015
+ * Song Hyeon Sik <blogcin@naver.com> 2016
  */
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"strconv"
 	"strings"
-	"io"
 )
 
 const (
 	bufferLength = 8192
-	headerLine = 30
+	headerLine   = 30
 )
 
+// ProxyServer stores the configuration for ProxyServer and some functions.
 type ProxyServer struct {
 	port string
 }
 
-func (ps *ProxyServer) askPort() int{
+// ask port
+func (ps *ProxyServer) askPort() int {
 	port := 0
 
 	fmt.Print("Port : ")
@@ -31,6 +33,7 @@ func (ps *ProxyServer) askPort() int{
 	return port
 }
 
+// initialize socket server 
 func (ps *ProxyServer) init(port int) net.Listener {
 	ps.port = ":"
 	ps.port += strconv.Itoa(port)
@@ -44,7 +47,8 @@ func (ps *ProxyServer) init(port int) net.Listener {
 	return server
 }
 
-func (ps *ProxyServer) acceptClient(server net.Listener) chan net.Conn{
+// accept client 
+func (ps *ProxyServer) acceptClient(server net.Listener) chan net.Conn {
 	channel := make(chan net.Conn)
 
 	go func() {
@@ -61,20 +65,20 @@ func (ps *ProxyServer) acceptClient(server net.Listener) chan net.Conn{
 	return channel
 }
 
+// connect host by socket
 func (ps *ProxyServer) connectHost(client net.Conn) {
 	HeaderInfo, Datas := ps.getData(client)
 
-	if(Datas[0] == 0 ) || (HeaderInfo == "-1"){
+	if (Datas[0] == 0) || (HeaderInfo == "-1") {
+		return
+	}
+	requestType, host, _, port := ps.parseHTTPHeaderMethod(HeaderInfo)
+
+	if port == -1 {
 		return
 	}
 
-	requestType, host, _, port := ps.parseHttpHeaderMethod(HeaderInfo)
-
-	if(port == -1) {
-		return
-	}
-
-	connectionHost, _ := net.Dial("tcp", host + ":"+ strconv.Itoa(port))
+	connectionHost, _ := net.Dial("tcp", host+":"+strconv.Itoa(port))
 
 	if requestType == "CONNECT" {
 		connectionHost.Write([]byte("HTTP/1.1 200 Connection established\n"))
@@ -90,27 +94,26 @@ func (ps *ProxyServer) connectHost(client net.Conn) {
 	return
 }
 
-/*
-Get first line of Http header
- */
-func (ps *ProxyServer) getData(client net.Conn) (string, []byte){
+// Get first line of http header
+func (ps *ProxyServer) getData(client net.Conn) (string, []byte) {
 
 	buffer := make([]byte, bufferLength)
 	client.Read(buffer)
 
-
 	return ps.splitHeader(buffer)[0], buffer
 }
 
-func (ps *ProxyServer) parseHttpHeaderMethod(headerMethod string) (string, string, string, int) {
+// parse HTTPHeader from packets
+func (ps *ProxyServer) parseHTTPHeaderMethod(headerMethod string) (string, string, string, int) {
 	var (
 		requestType string
-		host string
-		protocol string
-		port int
+		host        string
+		protocol    string
+		port        int
 	)
-	// ex: GET http://google.com/ HTTP/1.1
+	errorCounter := false
 
+	// ex: GET http://google.com/ HTTP/1.1
 	temp := headerMethod[strings.Index(headerMethod, " ")+1:]
 	protocol = temp[strings.Index(temp, " ")+1:]
 
@@ -120,66 +123,65 @@ func (ps *ProxyServer) parseHttpHeaderMethod(headerMethod string) (string, strin
 
 	if i == -1 {
 		fmt.Println("Uncorrect URL")
-		return "", "", "", -1
+		errorCounter = true
 	} else {
-		host = url[i+3:len(url)-1]
+		host = url[i+3 : len(url)-1]
 	}
 
-	i = strings.Index(host, ":")
-	if i == -1 {
-		port = 80
-	} else {
-		port, _ = strconv.Atoi(host[i+1:])
-		host = host[:i]
+	if errorCounter == false {
+		i = strings.Index(host, ":")
+		if i == -1 {
+			port = 80
+		} else {
+			port, _ = strconv.Atoi(host[i+1:])
+			host = host[:i]
+		}
+
+		i = strings.Index(host, "/")
+		if i != -1 {
+			host = host[:i]
+		}
+		requestType = headerMethod[:strings.Index(headerMethod, " ")]
+
+		return requestType, host, protocol, port
 	}
 
-	i = strings.Index(host, "/")
-	if i != -1 {
-		host = host[:i]
-	}
-	requestType = headerMethod[:strings.Index(headerMethod, " ")]
-
-	return requestType, host, protocol, port
+	return "", "", "", -1
 }
 
+// split header each lines
 func (ps *ProxyServer) splitHeader(bytearray []byte) []string {
 
 	result := make([]string, headerLine)
 	j := 0
 	temp := false
 
-	if(bytearray[0] == 0) {
+	if bytearray[0] == 0 {
 		fmt.Println("ps: splitHeader: Couldn't get httpheader, zero filter")
 		result[0] = string("-1")
 		return result
 	}
 
 	for index, element := range bytearray {
-		if(element == '\r') {
-			if(bytearray[index+1] == '\n') {
+		if element == '\r' {
+			if bytearray[index+1] == '\n' {
 				temp = true
 			}
 		}
 
-		if(temp != true) {
+		if temp != true {
 			result[j] += string(element)
 		}
 
-		if(element == '\n') {
+		if element == '\n' {
 			temp = false
-			j += 1
+			j++
 		}
 	}
 
-	return result;
+	return result
 }
 
-
-/*
-func byteArrtoStr(byteArray []byte) string{
-	return string(byteArray[:])
-}
-*/
 func main() {
 	proxyServer := &ProxyServer{}
 
